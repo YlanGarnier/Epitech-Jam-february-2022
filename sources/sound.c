2040300d2b88c3b8d3bd7417f8813924ce2a07e0
Yahia-Abdchafee-ADAM <adam.yahi-abdchafee@epitech.eu>
2022-02-12 17:16:43 +0100

    [ADD] sound and music

--- sources/sound.c
+++ sources/sound.c
@@ -7,9 +7,25 @@
 
 #include "game.h"
 
+
+void music(sound_td *s)
+{
+    s->musique = sfMusic_createFromFile("assets/music1.ogg");
+    sfMusic_setLoop(s->musique, sfTrue);
+    sfMusic_play(s->musique);
+}
+
+void init_sound(sound_td *s)
+{
+    s->sound_buff = sfSoundBuffer_createFromFile("assets/son.ogg");
+    s->sound = sfSound_create();
+    sfSound_setBuffer(s->sound, s->sound_buff);
+    return;
+}
+
 void destroy_sounds(sound_td *sound)
 {
-    //sfMusic_destroy(sound->leson);
+    sfMusic_destroy(sound->musique);
     //sfSound_destroy(sound->leson);
     //sfSoundBuffer_destroy(sound->lebuffer_duson);
     return;
